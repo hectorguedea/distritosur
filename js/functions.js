8cb28e81cf3d6119560b454baa9f86e547369567
Héctor Guedea <hectorguedea@gmail.com>
2024-11-15 17:15:02 -0600

    Inicio de proyecto

---
+++ js/functions.js
@@ -0,0 +1,9 @@
+/*
+jQuery Functions
+Héctor Guedea
+24 octubre, 2024
+*/
+
+$(document).ready(function() {
+    console.log("El documento está listo y jQuery está funcionando correctamente.");
+});
